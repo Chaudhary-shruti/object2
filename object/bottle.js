9a3636a41bb3148ffc2e9b119ed24e3ba91f0186
Chaudhary-shruti <93135302+Chaudhary-shruti@users.noreply.github.com>
2023-02-25 21:43:07 -0800

    Add files via upload

---
+++ object/bottle.js
@@ -0,0 +1,14 @@
+function preLoad(){
+    img=loadImage("bottle.jfif")
+}
+function setup(){
+canvas=createCanvas(640,420)
+canvas.center()
+
+}
+function modelLoaded(){
+
+}
+function draw(){
+
+}
---
+++ object/pen.js
@@ -0,0 +1,14 @@
+function preLoad(){
+    img=loadImage("pen.png")
+}
+function setup(){
+canvas=createCanvas(640,420)
+canvas.center()
+
+}
+function modelLoaded(){
+
+}
+function draw(){
+
+}
